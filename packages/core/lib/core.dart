4862fa776c111040e62c8c73cf789df2ea0f4575
Adby Santos <adby.santos15@outlook.com>
2022-10-15 19:45:43 -0300

    chore: organizing core

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -1,6 +1,6 @@
 library core;
 
-export 'src/http_service.dart';
-export 'src/http_response.dart';
+export 'src/http/http_service.dart';
+export 'src/http/http_response.dart';
 export 'src/dependency_injection/dependency_injection_widget.dart';
 export 'src/dependency_injection/feature.dart';
