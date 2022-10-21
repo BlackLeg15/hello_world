11d36142fb1b7c01287e76912fa9c30ab9f85177
Adby Santos <adby.santos15@outlook.com>
2022-10-21 10:39:53 -0300

    feat: poke list usecase's test

---
+++ packages/poke_list/test/src/domain/usecases/search_pokemon_usecase_impl_test.dart
@@ -0,0 +1,34 @@
+import 'package:dependencies/dependencies.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:poke_list/src/domain/entities/search_pokemon_entity.dart';
+import 'package:poke_list/src/domain/params/search_pokemon_params.dart';
+import 'package:poke_list/src/domain/repositories/search_pokemon_repository.dart';
+import 'package:poke_list/src/domain/usecases/search_pokemon_usecase_impl.dart';
+
+class SearchPokemonRepositoryMock extends Mock implements SearchPokemonRepository {}
+
+void main() {
+  late SearchPokemonRepository repository;
+  late SearchPokemonUsecaseImpl searchPokemonUsecaseImpl;
+
+  setUp(() {
+    repository = SearchPokemonRepositoryMock();
+    searchPokemonUsecaseImpl = SearchPokemonUsecaseImpl(repository);
+    registerFallbackValue(const SearchPokemonParams(name: 'whatever'));
+  });
+
+  group('Search Pokemon Usecase', () {
+    test('working correctly', () async {
+      when(() => repository(any())).thenAnswer(
+        (invocation) async => const Right(
+          SearchPokemonEntity(id: '0', name: 'any', abilities: []),
+        ),
+      );
+
+      final result = await searchPokemonUsecaseImpl(const SearchPokemonParams(name: 'ditto'));
+
+      expect(result, isA<Right>());
+    });
+  });
+}
