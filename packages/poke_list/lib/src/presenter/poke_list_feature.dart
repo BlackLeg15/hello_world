3c318d619a58c7c5fad2b83386283eba6a15afb6
Adby Santos <adby.santos15@outlook.com>
2022-12-12 10:18:36 -0300

    chore: organizing

--- packages/poke_list/lib/src/presenter/poke_list_feature.dart
+++ packages/poke_list/lib/src/presenter/poke_list_feature.dart
@@ -2,7 +2,6 @@ import 'package:core/core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:poke_list/src/presenter/pages/form_controller.dart';
 import 'package:poke_list/src/presenter/poke_list_controller.dart';
-import 'package:poke_list/src/presenter/reactive/mobx_reactive_provider.dart';
 import 'package:poke_list/src/presenter/reactive/reactive_provider.dart';
 import 'package:poke_list/src/presenter/stores/search_pokemon_store/search_pokemon_store.dart';
 import 'package:uno_http_service/uno_http_service.dart';
@@ -12,6 +11,7 @@ import '../external/datasources/api_v2/search_pokemon_api_v2_datasource.dart';
 import '../external/datasources/api_v2/search_pokemon_api_v2_mapper.dart';
 import '../infra/repositories/search_pokemon_repository_impl.dart';
 import 'poke_list_page.dart';
+import 'reactive/mobx/mobx_reactive_provider.dart';
 
 class PokeListFeature extends Feature {
   @override
--- packages/poke_list/lib/src/presenter/reactive/mobx/mobx_reactive_provider.dart
+++ packages/poke_list/lib/src/presenter/reactive/mobx/mobx_reactive_provider.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:poke_list/src/presenter/reactive/mobx_reactive_widget.dart';
+import 'package:flutter/widgets.dart';
 import 'package:poke_list/src/presenter/reactive/reactive_provider.dart';
 import 'package:poke_list/src/presenter/reactive/reactive_widget.dart';
+
+import 'mobx_reactive_widget.dart';
 
 class MobxReactiveProvider extends ReactiveProvider {
   const MobxReactiveProvider();
